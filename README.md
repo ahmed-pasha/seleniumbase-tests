9f190a2bd89c928eac536d31b84f515b3ff774c1
ahmed-glitch07 <pashaahmed162@gmail.com>
2024-07-18 07:22:18 +0530

    Initial commit with SeleniumBase tests

---
+++ README.md
@@ -0,0 +1,10 @@
+# SeleniumBase Tests
+
+This repository contains automated tests using SeleniumBase for testing various functionalities on the "the-internet.herokuapp.com" website.
+
+## Setup
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/ahmed-pasha/seleniumbase-tests
+   cd seleniumbase-tests
---
+++ tests.py
@@ -0,0 +1,59 @@
+from seleniumbase import BaseCase
+
+class AdditionalTheInternetTests(BaseCase):
+
+    def test_form_authentication(self):
+        self.open("https://the-internet.herokuapp.com/login")
+        self.type("#username", "tomsmith")
+        self.type("#password", "SuperSecretPassword!")
+        self.click("button[type='submit']")
+        self.assert_text("Secure Area", "h2")
+        self.assert_element(".flash.success")
+
+    def test_file_upload(self):
+        self.open("https://the-internet.herokuapp.com/upload")
+        self.choose_file("#file-upload", "C:\\Users\\pasha\\Desktop\\file.txt")
+        self.click("#file-submit")
+        self.assert_text("File Uploaded!", "h3")
+        self.assert_element("#uploaded-files")
+
+    def test_javascript_alerts(self):
+        self.open("https://the-internet.herokuapp.com/javascript_alerts")
+        
+        self.click("button[onclick='jsAlert()']")
+        self.wait_for_alert(timeout=20)  # Increase timeout to 20 seconds
+        self.accept_alert()
+        self.assert_text("You successfully clicked an alert", "#result")
+
+        self.click("button[onclick='jsConfirm()']")
+        self.wait_for_alert(timeout=20)  # Increase timeout to 20 seconds
+        self.dismiss_alert()
+        self.assert_text("You clicked: Cancel", "#result")
+
+        self.click("button[onclick='jsPrompt()']")
+        self.wait_for_alert(timeout=20)  # Increase timeout to 20 seconds
+        self.send_keys_to_alert("SeleniumBase")
+        self.accept_alert()
+        self.assert_text("You entered: SeleniumBase", "#result")
+
+    def test_basic_auth(self):
+        self.open("https://admin:admin@the-internet.herokuapp.com/basic_auth")
+        self.assert_text("Congratulations! You must have the proper credentials.", "p")
+
+    def test_dynamic_controls(self):
+        self.open("https://the-internet.herokuapp.com/dynamic_controls")
+        self.click("#checkbox-example button")
+        self.assert_text("It's gone!", "#message")
+
+        self.click("#checkbox-example button")
+        self.assert_element("#checkbox")
+
+        self.click("#input-example button")
+        self.assert_element("#input-example input:enabled")
+
+        self.click("#input-example button")
+        self.assert_element("#input-example input:disabled")
+
+if __name__ == "__main__":
+    from pytest import main
+    main(["-v", "tests.py"])
