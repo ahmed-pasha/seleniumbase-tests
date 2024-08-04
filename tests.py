41540e33c04f9f814da36784345063e24538ee5d
Ahmed pasha <151767359+ahmed-pasha@users.noreply.github.com>
2024-08-05 00:58:32 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,62 +1,63 @@
 from seleniumbase import BaseCase
+import time
 
-class AdditionalTheInternetTests(BaseCase):
+class InternetHerokuappTests(BaseCase):
 
-    def test_form_authentication(self):
+    def test_login(self):
         self.open("https://the-internet.herokuapp.com/login")
+        time.sleep(1)  # Adding delay
         self.type("#username", "tomsmith")
+        time.sleep(1)  # Adding delay
         self.type("#password", "SuperSecretPassword!")
+        time.sleep(1)  # Adding delay
         self.click("button[type='submit']")
-        self.assert_text("Secure Area", "h2")
-        self.assert_element(".flash.success")
+        time.sleep(1)  # Adding delay
+        self.assert_text("You logged into a secure area!", "#flash")
+
+    def test_add_remove_elements(self):
+        self.test_login()  # Ensure login is done first
+        self.open("https://the-internet.herokuapp.com/add_remove_elements/")
+        time.sleep(1)  # Adding delay
+        self.click("button[onclick='addElement()']")
+        time.sleep(1)  # Adding delay
+        self.assert_element_present(".added-manually")
+        time.sleep(1)  # Adding delay
+        self.click(".added-manually")
+        time.sleep(1)  # Adding delay
+        self.assert_element_not_present(".added-manually")
+
+    def test_checkboxes(self):
+        self.test_login()  
+        self.open("https://the-internet.herokuapp.com/checkboxes")
+        time.sleep(1)  # Adding delay
+        self.assert_false(self.is_checked("#checkboxes input:nth-child(1)"))
+        time.sleep(1)  # Adding delay
+        self.click("#checkboxes input:nth-child(1)")
+        time.sleep(1)  # Adding delay
+        self.assert_true(self.is_checked("#checkboxes input:nth-child(1)"))
+        time.sleep(1)  # Adding delay
+        self.assert_true(self.is_checked("#checkboxes input:nth-child(3)"))
+        time.sleep(1)  # Adding delay
+        self.click("#checkboxes input:nth-child(3)")
+        time.sleep(1)  # Adding delay
+        self.assert_false(self.is_checked("#checkboxes input:nth-child(3)"))
+
+    def test_context_menu(self):
+        self.test_login()  
+        self.open("https://the-internet.herokuapp.com/context_menu")
+        time.sleep(1)  # Adding delay
+        self.context_click("#hot-spot")
+        time.sleep(1)  
+        self.assert_alert_text("You selected a context menu")
+        self.accept_alert()
 
     def test_file_upload(self):
+        self.test_login() 
         self.open("https://the-internet.herokuapp.com/upload")
-        self.choose_file("#file-upload", "C:\\Users\\pasha\\Desktop\\file.txt")
+        time.sleep(1) 
+        self.choose_file("#file-upload", "sample.txt")
+        time.sleep(1) 
         self.click("#file-submit")
+        time.sleep(1) 
         self.assert_text("File Uploaded!", "h3")
-        self.assert_element("#uploaded-files")
 
-    def test_javascript_alerts(self):
-        self.open("https://the-internet.herokuapp.com/javascript_alerts")
-        
-        self.click("button[onclick='jsAlert()']")
-        self.wait_for_alert(timeout=20)
-        self.accept_alert()
-        self.assert_text("You successfully clicked an alert", "#result")
-
-        self.click("button[onclick='jsConfirm()']")
-        self.wait_for_alert(timeout=20)
-        self.dismiss_alert()
-        self.assert_text("You clicked: Cancel", "#result")
-
-        self.click("button[onclick='jsPrompt()']")
-        self.wait_for_alert(timeout=20)
-        self.send_keys_to_alert("SeleniumBase")
-        self.accept_alert()
-        self.assert_text("You entered: SeleniumBase", "#result")
-
-    def test_basic_auth(self):
-        self.open("https://admin:admin@the-internet.herokuapp.com/basic_auth")
-        self.assert_text("Congratulations! You must have the proper credentials.", "p")
-
-    def test_dynamic_controls(self):
-        self.open("https://the-internet.herokuapp.com/dynamic_controls")
-        self.click("#checkbox-example button")
-        self.assert_text("It's gone!", "#message")
-
-        # This step will fail intentionally
-        self.assert_element("#checkbox")  # This element should not exist
-
-        self.click("#checkbox-example button")
-        self.assert_element("#checkbox")
-
-        self.click("#input-example button")
-        self.assert_element("#input-example input:enabled")
-
-        self.click("#input-example button")
-        self.assert_element("#input-example input:disabled")
-
-if __name__ == "__main__":
-    from pytest import main
-    main(["-v", "tests.py"])
